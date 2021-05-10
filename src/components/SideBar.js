36d8879be81b9bd3af5b537acda7e0ba998e96bd
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-05-10 17:26:45 +0500

    add side ui

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,24 +2,52 @@ import React from "react";
 import styled from "styled-components";
 import Compose from "./Button/Compose";
 import { sidebarButtonItems } from "./data/SideBarButtonItems";
-
+import VideoCamIcon from "@material-ui/icons/Videocam";
+import KeyboardIcon from "@material-ui/icons/Keyboard";
+import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircleOutlined";
+import { BottomIcons } from "./data/BottomIcons";
 function SideBar() {
   return (
     <Wrapper>
-      <ComposeWrapper>
-        <Compose />
-      </ComposeWrapper>
-      <SideButtonWrapper>
-        {sidebarButtonItems.map((btn) => (
-          <SideBarButtonItem>
-            {btn.icon}
-            {btn.text}
-          </SideBarButtonItem>
-        ))}
-      </SideButtonWrapper>
-      <MeetWrapper></MeetWrapper>
-      <HangOutWrapper></HangOutWrapper>
-      <bottomIconsWrapper></bottomIconsWrapper>
+      <TopSectionWrapper>
+        <ComposeWrapper>
+          <Compose />
+        </ComposeWrapper>
+        <SideButtonWrapper>
+          {sidebarButtonItems.map((btn) => (
+            <SideBarButtonItem>
+              {btn.icon}
+              {btn.text}
+            </SideBarButtonItem>
+          ))}
+        </SideButtonWrapper>
+      </TopSectionWrapper>
+      <BottomBarSectionWrapper>
+        <SidebarSelectionWrapper>
+          <Title>Meet</Title>
+          <p>
+            <VideoCamIcon />
+            New Meeting
+          </p>
+          <p>
+            <KeyboardIcon />
+            join Meeting
+          </p>
+        </SidebarSelectionWrapper>
+        <SidebarSelectionWrapper>
+          <Title>Hongs Out</Title>
+          <p>
+            <AccountCircleOutlinedIcon />
+            New Meeting
+          </p>
+        </SidebarSelectionWrapper>
+
+        <BottomIconsWrapper>
+          {BottomIcons.map((icon) => (
+            <>{icon}</>
+          ))}
+        </BottomIconsWrapper>
+      </BottomBarSectionWrapper>
     </Wrapper>
   );
 }
@@ -28,7 +56,10 @@ export default SideBar;
 
 const Wrapper = styled.div`
   border-right: 1px solid lightgrey;
-  height: 100vh;
+  height: calc(100vh - 70px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const ComposeWrapper = styled.div`
@@ -36,10 +67,38 @@ const ComposeWrapper = styled.div`
   place-items: start;
   padding: 10px 20px;
 `;
-
+const BottomIconsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  color: gray;
+  border-top: 1px solid lightgrey;
+  cursor: pointer;
+  margin-top: 60px;
+  .MuiSvgIcon-root {
+    padding: 2px;
+  }
+`;
+const TopSectionWrapper = styled.div``;
 const SideButtonWrapper = styled.div``;
 
-const MeetWrapper = styled.div``;
+const BottomBarSectionWrapper = styled.div`
+  margin-bottom: 10px;
+`;
+
+const SidebarSelectionWrapper = styled.div`
+  border-top: 1px solid lightgrey;
+  p {
+    color: gray;
+    display: grid;
+    grid-template-columns: 40px auto;
+    padding: 5px 25px;
+  }
+`;
+const Title = styled.h4`
+  padding-left: 25px;
+  margin-bottom: 3px;
+  margin-top: 8px;
+`;
 
 const HangOutWrapper = styled.div``;
 const SideBarButtonItem = styled.div`
@@ -54,5 +113,3 @@ const SideBarButtonItem = styled.div`
     background-color: #f5f7f7;
   }
 `;
-
-const bottomIconsWrapper = styled.div``;
---
+++ src/components/data/BottomIcons.js
@@ -0,0 +1,4 @@
+import PersonIcon from "@material-ui/icons/Person";
+import PhoneIcon from "@material-ui/icons/Phone";
+import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
+export const BottomIcons = [<PersonIcon />, <PhoneIcon />, <FormatQuoteIcon />];
