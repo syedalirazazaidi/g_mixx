a552e354104e6dfecb61ae5146d53781b4d9106b
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-05-11 04:53:33 +0500

    add mail Ui with ripple effect

--- src/components/EmailsView.js
+++ src/components/EmailsView.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import styled from "styled-components";
+import Checkbox from "@material-ui/core/Checkbox";
+import RefreshIcon from "@material-ui/icons/Refresh";
+
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+import IconButton from "@material-ui/core/IconButton";
+
 function EmailsView() {
-  return <div>Email</div>;
+  return (
+    <Wrapper>
+      <TopWrapper>
+        <Checkbox />
+        <IconButton>
+          <RefreshIcon />
+        </IconButton>
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
+      </TopWrapper>
+      <EmailContainer></EmailContainer>
+    </Wrapper>
+  );
 }
 
 export default EmailsView;
+const Wrapper = styled.div``;
+
+const TopWrapper = styled.div``;
+
+const EmailContainer = styled.div``;
--- src/components/SideIcon.js
+++ src/components/SideIcon.js
@@ -1,7 +1,35 @@
 import React from "react";
-
+import styled from "styled-components";
+import AddIcon from "@material-ui/icons/Add";
 function SideIcon() {
-  return <div>SideIcon</div>;
+  return (
+    <Wrapper>
+      <img
+        src="https://cdn4.iconfinder.com/data/icons/logos-brands-in-colors/48/google-calendar-512.png"
+        alt="ima"
+      />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Google_Keep_icon_%282020%29.svg/1200px-Google_Keep_icon_%282020%29.svg.png" />
+      <img src="https://www.androidpolice.com/wp-content/uploads/2018/03/nexus2cee_new-tasks-icon.png" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Google_Contacts_icon.svg/1200px-Google_Contacts_icon.svg.png" />
+      <hr />
+      <AddIcon />
+    </Wrapper>
+  );
 }
 
 export default SideIcon;
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: repeat(4, 50px) 1px 50px;
+  place-items: center;
+  border-left: 1px solid lightgrey;
+  img {
+    width: 100%;
+    padding: 4px;
+    max-height: 22px;
+    object-fit: contain;
+  }
+  hr {
+    width: 60%;
+  }
+`;
